daf7ad45f02daed3d88e0fc8fd553879aeca65de
Devin Breen <dbreen@groupon.com>
2015-11-03 19:08:56 -0600

    Auth works

--- app/src/main/java/com/codepath/apps/twitterclient/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/twitterclient/TwitterClient.java
@@ -1,14 +1,14 @@
 package com.codepath.apps.twitterclient;
 
-import org.scribe.builder.api.Api;
-import org.scribe.builder.api.FlickrApi;
-
 import android.content.Context;
 
+import com.codepath.apps.twitterclient.config.TwitterConfig;
 import com.codepath.oauth.OAuthBaseClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TwitterApi;
 /*
  * 
  * This is the object responsible for communicating with a REST API. 
@@ -21,14 +21,14 @@ import com.loopj.android.http.RequestParams;
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
  * 
  */
-public class RestClient extends OAuthBaseClient {
-	public static final Class<? extends Api> REST_API_CLASS = FlickrApi.class; // Change this
-	public static final String REST_URL = "http://api.flickr.com/services"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
-	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
-	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
-
-	public RestClient(Context context) {
+public class TwitterClient extends OAuthBaseClient {
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static final String REST_CONSUMER_KEY = TwitterConfig.TWITTER_KEY;
+	public static final String REST_CONSUMER_SECRET = TwitterConfig.TWITTER_SECRET;
+    public static final String REST_CALLBACK_URL = "oauth://cptwitterclient";
+    
+	public TwitterClient(Context context) {
 		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
 	}
 
---
+++ app/src/main/java/com/codepath/apps/twitterclient/config/TwitterConfig.java
@@ -0,0 +1,9 @@
+package com.codepath.apps.twitterclient.config;
+
+/**
+ * Created by devin on 11/3/15.
+ */
+public class TwitterConfig {
+    public static final String TWITTER_KEY = "";
+    public static final String TWITTER_SECRET = "";
+}
